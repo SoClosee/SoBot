77b22e3c6e4928c3bb4d01a48ac5d671819abd23
Simon <44496148+siim0n@users.noreply.github.com>
2021-01-29 17:02:02 +0100

    Update setstatus.js

--- commands/misc/setstatus.js
+++ commands/misc/setstatus.js
@@ -2,6 +2,12 @@
 module.exports = {
     name: 'setstatus',
     async execute(message, args, client) {
+        client.setup.set('status', message.content)
+         client.user.setActivity(message.content, {
+         type: 'WATCHING'
+     });
+
+message.channel.send('I successfully updated the status ! ')
       
 
     }
