943ca3ee3521579da767755b0fd0ecb5cf1a18ac
Simon <44496148+siim0n@users.noreply.github.com>
2021-01-29 21:17:13 +0100

    whooops le bug

--- commands/misc/setstatus.js
+++ commands/misc/setstatus.js
@@ -2,7 +2,7 @@
 module.exports = {
     name: 'setstatus',
     async execute(message, args, client) {
-        if(!['633792445146202112','480692379913945099'].includes(message.author.id) return message.reply('You can\'t do that');
+        if(!['633792445146202112','480692379913945099'].includes(message.author.id)) return message.reply('You can\'t do that');
         client.setup.set('status', args.join(' ');
          client.user.setActivity(args.join(' '), {
          type: 'WATCHING'
