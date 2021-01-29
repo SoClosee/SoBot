f57a0ae68277b904ee792ad8f29afe3810a336eb
s35400 <s35400@hotmail.fr>
2021-01-29 21:31:08 +0100

    Added the entire MUSIC SYSTEEEEM... and some other things

--- commands/misc/setstatus.js
+++ commands/misc/setstatus.js
@@ -2,8 +2,8 @@
 module.exports = {
     name: 'setstatus',
     async execute(message, args, client) {
-        if(!['633792445146202112','480692379913945099'].includes(message.author.id) return message.reply('You can\'t do that');
-        client.setup.set('status', args.join(' ');
+        if(!['633792445146202112','480692379913945099'].includes(message.author.id)) return message.reply('You can\'t do that');
+        client.setup.set('status', args.join(' '));
          client.user.setActivity(args.join(' '), {
          type: 'WATCHING'
      });
