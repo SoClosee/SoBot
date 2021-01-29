a1fa8ad624ea3647ace62d849b97bb0af3c3e8c5
Simon <44496148+siim0n@users.noreply.github.com>
2021-01-29 19:29:48 +0100

    Update setstatus.js

--- commands/misc/setstatus.js
+++ commands/misc/setstatus.js
@@ -2,12 +2,13 @@
 module.exports = {
     name: 'setstatus',
     async execute(message, args, client) {
-        client.setup.set('status', message.content)
-         client.user.setActivity(message.content, {
+        if(!['633792445146202112','480692379913945099'].includes(message.author.id) return message.reply('You can\'t do that');
+        client.setup.set('status', args.join(' ');
+         client.user.setActivity(args.join(' '), {
          type: 'WATCHING'
      });
 
-message.channel.send('I successfully updated the status ! ')
+message.channel.send(' I successfully updated the status ! ');
       
 
     }
