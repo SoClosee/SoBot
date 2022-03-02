5c0d540f2cc1dea9b10a3244baba12e81dd8f259
Simon <44496148+siim0n@users.noreply.github.com>
2022-03-02 21:57:31 +0100

    Update buy.js

--- slash/buy.js
+++ slash/buy.js
@@ -53,7 +53,7 @@ module.exports = {
         }
         catch (err) {
             console.log(err)
-            interaction.followUp('Timeout / Problem occured')
+            interaction.followUp('Timeout / Problem occured', {ephemeral: true})
         }
 
 
