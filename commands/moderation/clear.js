07f66a42612d7f2a12650e6b59c9334553fda45a
Simon <44496148+siim0n@users.noreply.github.com>
2022-04-25 20:49:51 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -11,7 +11,7 @@ module.exports = {
                 
                let limit = parseInt(numberOfMessages)
             await   message.channel.messages.fetch({limit: limit}).then((async messages=>{
-                  await messages.map((totalMessage) => {
+                  await messages.map(async (totalMessage) => {
                       toIncrement++
                     await totalMessage.delete({timeout: 2300}) 
                    })
