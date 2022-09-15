2863f983f0e80f91f647bc2b6a7caa99f8805f79
Simon <44496148+siim0n@users.noreply.github.com>
2022-09-15 21:11:55 +0200

    Removed 'remove role' on add

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -57,52 +57,11 @@ module.exports = async (client, reaction, user) => {
     }
     }else  if (!reaction.message.guild.me.permissions.has('MANAGE_ROLES')) return;
     
-    let gID = reaction.message.guild.id
-    if (client.setup.has(gID, 'reactionrole')) {
-      
-        if (client.setup.has(gID, `reactionrole.${reaction.message.id}`)) {
+    
+                
             
-            stuff = await client.setup.get(gID, 'reactionrole.' + reaction.message.id)
-console.log(reaction)
-            if (reaction.emoji.id) {
-
-
-                for (t in stuff) {
-                    let emojidb = Object.values(client.setup.get(gID, `reactionrole.${reaction.message.id}.${t}`))
-                    if (emojidb[3] == reaction.emoji.id) {
-                        reaction.message.guild.members.fetch(user.id).then(member => {
-
-                            if(member.roles.cache.has(emojidb[2]))
-
-                                member.roles.remove(emojidb[2])
-
-                        })
-
-
-                    }
-                }
-
-
-
-
-            } else {
-                for (t in stuff) {
-                    let emojidb = Object.values(client.setup.get(gID, `reactionrole.${reaction.message.id}.${t}`))
-                    if (emojidb[3] == reaction.emoji.name) {
-                        reaction.message.guild.members.fetch(user.id).then(member => {
-
-                            if(member.roles.cache.has(emojidb[2]))
-
-                                member.roles.remove(emojidb[2])
-
-                        })
-
-
-                    }
-                }
-            }
-        }
-    }
+        
+    
     
 
 }
